9eb5ca393f109f6e7eee533abd15739080ee4176
Noom <joaquin12345.007.2002@gmail.com>
2023-08-06 14:24:53 -0500

    btn Eliminar Alumno

--- app/src/main/java/com/example/app_inf/MainActivity.kt
+++ app/src/main/java/com/example/app_inf/MainActivity.kt
@@ -6,6 +6,7 @@ import android.content.pm.ActivityInfo
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.ComponentActivity
+import com.example.app_inf.Activities.AlumnosActivity
 import com.example.app_inf.Activities.AsesoresActivity
 
 class MainActivity : ComponentActivity() {
@@ -15,10 +16,14 @@ class MainActivity : ComponentActivity() {
         setContentView(R.layout.main_activity)
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
         val btnAsesores = findViewById<Button>(R.id.btnAsesores)
+        val btnAlumnos = findViewById<Button>(R.id.btnAlumnos)
         btnAsesores.setOnClickListener {
             val intent = Intent(this, AsesoresActivity::class.java)
             startActivity(intent)
         }
+        btnAlumnos.setOnClickListener {
+            val intent = Intent(this, AlumnosActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
-
