c3dcce6e76f791b86fd3afded6ca89095807494f
MaxCastro2904 <118504584+MaxCastro2904@users.noreply.github.com>
2023-08-06 23:22:26 -0500

    btn_asesoralumno

--- app/src/main/java/com/example/app_inf/Activities/PaquetesActivity.kt
+++ app/src/main/java/com/example/app_inf/Activities/PaquetesActivity.kt
@@ -12,6 +12,11 @@ class PaquetesActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_paquetes)
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        val btn_back_alumno1 = findViewById<Button>(R.id.btn_back_alumno1)
+        btn_back_alumno1.setOnClickListener {
+            val intent = Intent(this, AgregarAlumnoActivity::class.java)
+            startActivity(intent)
+        }
         val btn_next_alumno2 = findViewById<Button>(R.id.btn_next_alumno2)
         btn_next_alumno2.setOnClickListener {
             val intent = Intent(this, AsesorAlumnoActivity::class.java)
