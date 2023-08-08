a785a8fece074920bb417419c8b6460d29f5e342
MaxCastro2904 <118504584+MaxCastro2904@users.noreply.github.com>
2023-08-08 18:47:48 -0500

    calendario

--- app/src/main/java/com/example/app_inf/Activities/CalendarioActivity.kt
+++ app/src/main/java/com/example/app_inf/Activities/CalendarioActivity.kt
@@ -8,6 +8,7 @@ import android.widget.TableRow
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.app_inf.R
+import java.util.Calendar
 
 class CalendarioActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,23 +28,40 @@ class CalendarioActivity : AppCompatActivity() {
         }
         tableLayout.addView(rowDays)
 
+        // Obtener el último día del año 2022 (Diciembre 31)
+        val calendar = Calendar.getInstance()
+        calendar.set(Calendar.YEAR, 2022)
+        calendar.set(Calendar.MONTH, Calendar.DECEMBER)
+        calendar.set(Calendar.DAY_OF_MONTH, 31)
+
+        // Avanzar hasta el primer lunes (día 2) de la última semana de diciembre
+        while (calendar.get(Calendar.DAY_OF_WEEK) != Calendar.MONDAY) {
+            calendar.add(Calendar.DAY_OF_MONTH, -1)
+        }
+
         // Agregar las columnas pares con números
-        var startNumber = 26
-        for (columnIndex in 0 until 107) {
-            val rowTableLayout = layoutInflater.inflate(R.layout.row_table, null) as TableRow
+        for (columnIndex in 0 until 106) {
             val isEvenColumn = columnIndex % 2 == 0
+
+            val rowTableLayout = TableRow(this)
 
             // Si la columna es par, agregar números
             if (isEvenColumn) {
                 for (i in 0 until 7) {
                     val textView = TextView(this)
-                    val dayNumber = if (columnIndex == 2 && i == 0) {
-                        // Special case for the first day in column 2
-                        startNumber++
-                    } else {
-                        (startNumber++).takeIf { it <= 31 } ?: 1
-                    }
+                    val dayNumber = getDayNumber(calendar)
                     textView.text = dayNumber.toString()
+                    textView.gravity = Gravity.CENTER
+                    rowTableLayout.addView(textView)
+
+                    // Avanzar al siguiente día
+                    calendar.add(Calendar.DAY_OF_MONTH, 1)
+                }
+            } else {
+                // Columnas impares muestran "maxxen"
+                for (i in 0 until 7) {
+                    val textView = TextView(this)
+                    textView.text = "maxxen"
                     textView.gravity = Gravity.CENTER
                     rowTableLayout.addView(textView)
                 }
@@ -57,5 +75,13 @@ class CalendarioActivity : AppCompatActivity() {
             rowTableLayout.layoutParams = params
             tableLayout.addView(rowTableLayout)
         }
+    }
+
+    private fun getDayNumber(calendar: Calendar): Int {
+        // Calcular el día del mes para imprimir
+        val dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH)
+
+        // Solo imprimir números válidos (hasta 31)
+        return dayOfMonth.takeIf { it <= 31 } ?: 1
     }
 }
