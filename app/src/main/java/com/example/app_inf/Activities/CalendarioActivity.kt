253c27eb1017f5d9912a33a684e6c0481974dae3
MaxCastro2904 <118504584+MaxCastro2904@users.noreply.github.com>
2023-08-08 10:23:48 -0500

    colors

---
+++ app/src/main/java/com/example/app_inf/Activities/CalendarioActivity.kt
@@ -0,0 +1,12 @@
+package com.example.app_inf.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.app_inf.R
+
+class CalendarioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calendario)
+    }
+}
