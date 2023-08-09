e06a7b292ef509936e11370457b906b785ab6f85
Noom <joaquin12345.007.2002@gmail.com>
2023-08-09 13:24:16 -0500

    calendario_V2

--- app/src/main/java/com/example/app_inf/Activities/CalendarioActivity.kt
+++ app/src/main/java/com/example/app_inf/Activities/CalendarioActivity.kt
@@ -1,16 +1,18 @@
 package com.example.app_inf.Activities
 
+import android.graphics.Color
 import android.os.Bundle
 import android.util.TypedValue
 import android.view.Gravity
-import android.widget.LinearLayout
 import android.widget.TableLayout
 import android.widget.TableRow
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.app_inf.R
 import java.util.Calendar
 import java.util.Date
+import java.util.Locale
 
 class CalendarioActivity : AppCompatActivity() {
     private val DAYS_OF_WEEK = arrayOf("Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo")
@@ -19,7 +21,10 @@ class CalendarioActivity : AppCompatActivity() {
     private val fechasPrueba = arrayOf(
         createDate(2023, 1, 29),
         createDate(2023, 1, 10),
-        createDate(2023, 1, 15)
+        createDate(2023, 1, 15),
+        createDate(2023, 3, 19),
+        createDate(2023, 2, 26)
+
         // Agrega más fechas aquí según necesites
     )
 
@@ -33,7 +38,6 @@ class CalendarioActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendario)
         val tableLayout = findViewById<TableLayout>(R.id.tl_month)
-        val monthNamesLayout = findViewById<LinearLayout>(R.id.month_names_layout)
 
         // Agregar días de la semana en la primera fila
         val rowDaysOfWeek = TableRow(this)
@@ -42,12 +46,6 @@ class CalendarioActivity : AppCompatActivity() {
             rowDaysOfWeek.addView(textView)
         }
         tableLayout.addView(rowDaysOfWeek)
-
-        val monthNames = arrayOf("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre")
-        for (monthName in monthNames) {
-            val monthTextView = createMonthTextView(monthName)
-            monthNamesLayout.addView(monthTextView)
-        }
 
         val calendar = Calendar.getInstance()
         calendar[Calendar.YEAR] = YEAR
@@ -59,7 +57,15 @@ class CalendarioActivity : AppCompatActivity() {
         val daysToAdjust = if (firstDayOfWeek == Calendar.SUNDAY) 6 else firstDayOfWeek - 2
         calendar.add(Calendar.DAY_OF_MONTH, -daysToAdjust)
 
+        var previousMonth = -1
+
         while (calendar[Calendar.YEAR] <= YEAR) {
+            val currentMonth = calendar[Calendar.MONTH]
+            if (previousMonth != currentMonth) {
+                tableLayout.addView(createMonthHeader(calendar))
+                previousMonth = currentMonth
+            }
+
             val rowDate = TableRow(this)
             for (i in 0 until DAYS_OF_WEEK.size) {
                 val dayNumber = calendar[Calendar.DAY_OF_MONTH]
@@ -81,6 +87,19 @@ class CalendarioActivity : AppCompatActivity() {
         markAttendance(tableLayout)
     }
 
+
+    private fun createMonthHeader(calendar: Calendar): TableRow {
+        val monthHeaderRow = TableRow(this)
+        val monthHeaderTextView = createTextView(calendar.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault()))
+        monthHeaderTextView.gravity = Gravity.CENTER
+        monthHeaderTextView.setBackgroundColor(ContextCompat.getColor(this, R.color.teal_200))
+        monthHeaderTextView.setTextColor(Color.WHITE)
+        monthHeaderTextView.layoutParams = TableRow.LayoutParams(0, TableRow.LayoutParams.WRAP_CONTENT, 1f)
+        monthHeaderRow.addView(monthHeaderTextView)
+        return monthHeaderRow
+    }
+
+
     private fun createTextView(text: String): TextView {
         val textView = TextView(this)
         textView.text = text
@@ -91,10 +110,8 @@ class CalendarioActivity : AppCompatActivity() {
         val paddingValue = resources.getDimensionPixelSize(R.dimen.text_view_padding)
         textView.setPadding(paddingValue, paddingValue, paddingValue, paddingValue)
 
-        val layoutParams = TableRow.LayoutParams(
-            TableRow.LayoutParams.WRAP_CONTENT,
-            TableRow.LayoutParams.WRAP_CONTENT
-        )
+        val layoutParams = TableRow.LayoutParams(250, TableRow.LayoutParams.WRAP_CONTENT, 1f)
+
 
         val marginValue = resources.getDimensionPixelSize(R.dimen.text_view_margin)
         layoutParams.setMargins(marginValue, marginValue, marginValue, marginValue)
@@ -107,15 +124,6 @@ class CalendarioActivity : AppCompatActivity() {
         return textView
     }
 
-    private fun createMonthTextView(text: String): TextView {
-        val textView = TextView(this)
-        textView.text = text
-        textView.gravity = Gravity.CENTER
-        textView.setBackgroundResource(R.drawable.cell_border)
-        textView.setPadding(8, 416, 8, 416)
-        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16F)
-        return textView
-    }
 
     private fun markAttendance(tableLayout: TableLayout) {
         for (fecha in fechasPrueba) {
@@ -123,22 +131,37 @@ class CalendarioActivity : AppCompatActivity() {
             calendar.time = fecha
             var weekIndex = calendar.get(Calendar.WEEK_OF_YEAR)
             var dayIndex = calendar.get(Calendar.DAY_OF_WEEK)
+            var cer = 0
 
             dayIndex = dayIndex - 2
+
             if(dayIndex < 0){
                 weekIndex -= 1
 
+
                 if(dayIndex == -1){
                     dayIndex = 6
+
                 }
                 else{
                     dayIndex = 5
                 }
             }
 
-            val rowAsistio = tableLayout.getChildAt(weekIndex * 2 + 2) as TableRow
+            fun calculateValue(input: Int): Int {
+                return when {
+                    input < 0 -> -1
+                    else -> input
+                }
+            }
+
+
+
+
+            val rowAsistio = tableLayout.getChildAt((calendar.get(Calendar.WEEK_OF_YEAR) * 3) + 3   ) as TableRow
             val textView = rowAsistio.getChildAt(dayIndex) as TextView
             textView.text = "Asistio"
         }
     }
+
 }
