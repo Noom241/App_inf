202c946e9bb60f708267f335e482010dc60335c0
Noom <joaquin12345.007.2002@gmail.com>
2023-08-09 19:45:50 -0500

    Calendario_c

--- app/src/main/java/com/example/app_inf/Activities/CalendarioActivity.kt
+++ app/src/main/java/com/example/app_inf/Activities/CalendarioActivity.kt
@@ -1,130 +1,98 @@
 package com.example.app_inf.Activities
-
+import com.example.app_inf.R
 import android.os.Bundle
-import android.util.TypedValue
 import android.view.Gravity
 import android.widget.TableLayout
 import android.widget.TableRow
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.example.app_inf.R
-import java.util.Calendar
-import java.util.Date
+import java.text.SimpleDateFormat
+import java.util.*
 
 class CalendarioActivity : AppCompatActivity() {
-    private val DAYS_OF_WEEK = arrayOf("Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo")
-    private val YEAR = 2023
 
-    private val fechasPrueba = arrayOf(
-        createDate(2023, 1, 29),
-        createDate(2023, 1, 10),
-        createDate(2023, 1, 29),
-        createDate(2023, 1, 10),
-        createDate(2023, 1, 15),
-        createDate(2023, 3, 19),
-        createDate(2023, 3, 24),
-        createDate(2023, 2, 16),
-        createDate(2023, 5, 18),
-        createDate(2023, 2, 26),
-        createDate(2023, 1, 15)
-        // Agrega más fechas aquí según necesites
-    )
-
-    private fun createDate(year: Int, month: Int, day: Int): Date {
-        val calendar = Calendar.getInstance()
-        calendar.set(year, month - 1, day) // Mes se cuenta desde 0 (enero)
-        return calendar.time
-    }
+    private var selectedYear = 2023
+    private var selectedMonth = Calendar.DECEMBER
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendario)
-        val tableLayout = findViewById<TableLayout>(R.id.tl_month)
-
-        // Agregar días de la semana en la primera fila
-        val rowDaysOfWeek = TableRow(this)
-        for (day in DAYS_OF_WEEK) {
-            val textView = createTextView(day)
-            rowDaysOfWeek.addView(textView)
-        }
-        tableLayout.addView(rowDaysOfWeek)
-
+        val monthTextView = findViewById<TextView>(R.id.monthTextView)
+        val tableLayout = findViewById<TableLayout>(R.id.tableLayout)
         val calendar = Calendar.getInstance()
-        calendar[Calendar.YEAR] = YEAR
-        calendar[Calendar.MONTH] = Calendar.JANUARY
-        calendar[Calendar.DAY_OF_MONTH] = 1
+        calendar.set(Calendar.YEAR, selectedYear)
+        calendar.set(Calendar.MONTH, selectedMonth)
+        calendar.set(Calendar.DAY_OF_MONTH, 1)
 
-        // Ajustar el calendario para mostrar días del año anterior si es necesario
-        val firstDayOfWeek = calendar[Calendar.DAY_OF_WEEK]
-        val daysToAdjust = if (firstDayOfWeek == Calendar.SUNDAY) 6 else firstDayOfWeek - 2
-        calendar.add(Calendar.DAY_OF_MONTH, -daysToAdjust)
+        val monthName = SimpleDateFormat("MMMM yyyy", Locale.getDefault()).format(calendar.time)
+        monthTextView.text = monthName
 
-        while (calendar[Calendar.YEAR] <= YEAR) {
-            val rowDate = TableRow(this)
-            val rowAsistio = TableRow(this) // Nueva fila para la asistencia
+        val diasSemana = arrayOf("Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado")
+        val primerDiaSemana = calendar.get(Calendar.DAY_OF_WEEK) - 1
 
-            for (i in 0 until DAYS_OF_WEEK.size) {
-                val dayNumber = calendar[Calendar.DAY_OF_MONTH]
-                val dayTextView = createTextView(dayNumber.toString())
-                val asistioTextView = createTextView("") // TextView para la asistencia
-                rowDate.addView(dayTextView)
-                rowAsistio.addView(asistioTextView)
-                calendar.add(Calendar.DAY_OF_MONTH, 1)
-            }
-            tableLayout.addView(rowDate)
-            tableLayout.addView(rowAsistio) // Agregar fila de asistencia
+        var dia = 1
+        var fila = TableRow(this)
+
+        val columnWidth = resources.getDimensionPixelSize(R.dimen.cell_width)
+
+        // Agregar encabezado de días de la semana
+        for (i in diasSemana.indices) {
+            val textView = TextView(this)
+            textView.text = diasSemana[i]
+            textView.gravity = Gravity.CENTER
+            textView.setBackgroundResource(R.drawable.cell_border)
+            textView.width = columnWidth
+            fila.addView(textView)
+        }
+        tableLayout.addView(fila)
+
+        fila = TableRow(this)
+
+        // Llenar las celdas vacías hasta el primer día de la semana
+        for (i in 0 until primerDiaSemana) {
+            val emptyCell = TextView(this)
+            emptyCell.text = " "
+            emptyCell.gravity = Gravity.CENTER
+            emptyCell.setBackgroundResource(R.drawable.cell_border)
+            emptyCell.width = columnWidth
+            fila.addView(emptyCell)
         }
 
-        // Marcar la asistencia
-        markAttendance(tableLayout)
-    }
+        while (calendar.get(Calendar.MONTH) == selectedMonth) {
+            val textView = TextView(this)
+            textView.text = dia.toString()
+            textView.gravity = Gravity.CENTER
+            textView.setBackgroundResource(R.drawable.cell_border)
+            textView.width = columnWidth
+            fila.addView(textView)
 
-
-    private fun createTextView(text: String): TextView {
-        val textView = TextView(this)
-        textView.text = text
-        textView.gravity = Gravity.CENTER
-        textView.setBackgroundResource(R.drawable.cell_border)
-
-        // Ajustar propiedades aquí
-        val paddingValue = resources.getDimensionPixelSize(R.dimen.text_view_padding)
-        textView.setPadding(paddingValue, paddingValue, paddingValue, paddingValue)
-
-        val layoutParams = TableRow.LayoutParams(0, TableRow.LayoutParams.WRAP_CONTENT, 1f)
-
-        val marginValue = resources.getDimensionPixelSize(R.dimen.text_view_margin)
-        layoutParams.setMargins(marginValue, marginValue, marginValue, marginValue)
-
-        val fontSize = resources.getDimensionPixelSize(R.dimen.text_view_font_size).toFloat()
-        textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, fontSize)
-
-        textView.layoutParams = layoutParams
-
-        return textView
-    }
-
-    private fun markAttendance(tableLayout: TableLayout) {
-        for (fecha in fechasPrueba) {
-            val calendar = Calendar.getInstance()
-            calendar.time = fecha
-            var weekIndex = calendar.get(Calendar.WEEK_OF_YEAR)
-            var dayIndex = calendar.get(Calendar.DAY_OF_WEEK)
-
-            dayIndex = dayIndex - 2
-            if(dayIndex < 0){
-                weekIndex -= 1
-
-                if(dayIndex == -1){
-                    dayIndex = 6
-                }
-                else{
-                    dayIndex = 5
-                }
+            if (calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY) {
+                tableLayout.addView(fila)
+                fila = TableRow(this)
             }
 
-            val rowAsistio = tableLayout.getChildAt(weekIndex * 2 + 2) as TableRow
-            val textView = rowAsistio.getChildAt(dayIndex) as TextView
-            textView.text = "Asistio"
+            calendar.add(Calendar.DAY_OF_MONTH, 1)
+            dia++
         }
+
+        // Llenar las celdas vacías restantes en la última fila
+        while (fila.childCount < 7) {
+            val emptyCell = TextView(this)
+            emptyCell.text = " "
+            emptyCell.gravity = Gravity.CENTER
+            emptyCell.setBackgroundResource(R.drawable.cell_border)
+            emptyCell.width = columnWidth
+            fila.addView(emptyCell)
+        }
+
+        if (fila.childCount > 0) {
+            tableLayout.addView(fila)
+        }
+    }
+
+    // Método para cambiar el mes seleccionado
+    private fun changeSelectedMonth(newMonth: Int) {
+        selectedMonth = newMonth
+        recreate() // Vuelve a crear la actividad para actualizar el calendario
     }
 }
